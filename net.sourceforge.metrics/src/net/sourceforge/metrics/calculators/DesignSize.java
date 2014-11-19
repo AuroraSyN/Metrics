1a81255f0a369a616cdcda9cd95944f6033455c1
Sumit Bisht <sumitbisht1987@gmail.com>
2014-11-19 18:50:57 +0530

    Updated Design size metric calculation

--- net.sourceforge.metrics/src/net/sourceforge/metrics/calculators/DesignSize.java
+++ net.sourceforge.metrics/src/net/sourceforge/metrics/calculators/DesignSize.java
@@ -1,8 +1,12 @@
 package net.sourceforge.metrics.calculators;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import net.sourceforge.metrics.core.Metric;
 import net.sourceforge.metrics.core.sources.AbstractMetricSource;
 import net.sourceforge.metrics.core.sources.ProjectMetrics;
+import net.sourceforge.metrics.core.sources.TypeMetrics;
 /**
  * Calculates the design complexity on the basis of number of classes in the application.
  * @author sumit bisht
@@ -17,13 +21,30 @@ public class DesignSize extends Calculator {
 	public void calculate(AbstractMetricSource source)
 			throws InvalidSourceException {
 		if (source.getLevel() != PROJECT){
-			System.out.println("ERROR: Unable to proceed as the selected element is not a project. Design size only applies to projects");
 			throw new InvalidSourceException("DesignSize metrics is only applicable to projects");
 		}
 		ProjectMetrics projMt = (ProjectMetrics) source;
-		int size = projMt.getChildren().size();
+		
+		int size = getAllTypes(projMt).size();
 		System.out.println("Project size: "+size);
 		source.setValue(new Metric(DESIGN_SIZE, size));
+	}
+	/**
+	 * @param source The root element for traversal
+	 * @return the type
+	 */
+	private List<TypeMetrics> getAllTypes(AbstractMetricSource source){
+		List <TypeMetrics> typeList = new ArrayList<TypeMetrics>();
+		if(source instanceof TypeMetrics)
+			typeList.add((TypeMetrics) source);
+		else if(source.getChildren().size()>0){
+			for(Object child : source.getChildren()){
+				List<TypeMetrics> childData = getAllTypes((AbstractMetricSource) child);
+				if(childData.size()>0)
+					typeList.addAll(childData);
+			}
+		}
+		return typeList;
 	}
 
 }
